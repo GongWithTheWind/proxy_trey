7f48e84d1d056be7a35739e146cb2998b9140b6b
rebz135 <rebz135@gmail.com>
2018-09-21 19:33:17 -0700

    Fix routing

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const proxy = require('http-proxy-middleware');
+const { routes } = require('./config.json');
 
-app.use(morgan('dev'));
-app.use('/home/:id', express.static(path.join(__dirname, 'public')));
+app.use('/home/:id', express.static('public'));
+
+for (route of routes) {
+    app.use(route.route,
+        proxy({
+            target: route.address
+            // pathRewrite: (path, req) => {
+            // 	console.log(route.address, path)
+            // }
+        })
+    );
+}
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
